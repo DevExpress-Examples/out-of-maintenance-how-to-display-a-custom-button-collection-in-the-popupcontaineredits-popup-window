8471314a76977c1c3630f44c22def31796c61bbe
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/RepositoryItemPopupContainerEditCustomButton.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.XtraEditors.Repository;
+using DevExpress.XtraEditors.Registrator;
+using DevExpress.XtraEditors.ViewInfo;
+using DevExpress.XtraEditors.Drawing;
+using System.ComponentModel;
+using DevExpress.XtraEditors.Controls;
+using DevExpress.XtraEditors;
+
+namespace PopupContainerCustomButtonControl
+{
+    internal class EventsArgsCustomButton : EventArgs
+    {
+        protected SimplePopupButton protCustomButton;
+        public SimplePopupButton CustomButton { get { return protCustomButton; } }
+        public EventsArgsCustomButton(SimplePopupButton currentButton)
+        {
+            this.protCustomButton = currentButton;
+        }
+    }
+
+    class RepositoryItemPopupContainerEditCustomButton : RepositoryItemPopupContainerEdit
+    {
+        // static constructor which calls static registration method
+        static RepositoryItemPopupContainerEditCustomButton() { RegisterPopupContainerEditCustomButton(); }
+
+        // static register method
+        public static void RegisterPopupContainerEditCustomButton()
+        {
+            EditorRegistrationInfo.Default.Editors.Add(new EditorClassInfo(
+                PopupContainerEditCustomButtonEditorName,
+                typeof(PopupContainerEditCustomButton),
+                typeof(RepositoryItemPopupContainerEditCustomButton),
+                typeof(PopupContainerEditViewInfo),
+                new ButtonEditPainter(),
+                true,
+                null));
+        }
+
+        // internal editor name
+        internal const string PopupContainerEditCustomButtonEditorName = "PopupContainerEditCustomButton";
+
+        // public constructor
+        public RepositoryItemPopupContainerEditCustomButton() : base()
+        {
+            protShowCustomButton = true;
+            this.protButtonsForPopup = new CustomButtonCollection();
+            protButtonsForPopup.OnCollectionChanged += new CustomButtonCollection.SimpleButtonCollectionEvenrHandler(ButtonsCollections_OnCollectionChanged);
+        }
+
+        void ButtonsCollections_OnCollectionChanged(object sender, CustomButtonCollectionEventArgs e)
+        {
+            if (e.CurrentAction == CollectionChangeAction.Add)
+                e.CustomButton.Click += new EventHandler(CustomButton_Click);
+            else
+                e.CustomButton.Click -= new EventHandler(CustomButton_Click);
+        }
+
+        void CustomButton_Click(object sender, EventArgs e)
+        {
+            if (CustomButtonClick != null) {
+                CustomButtonClick(this, new EventsArgsCustomButton(sender as SimplePopupButton));
+            }
+        }
+
+        protected bool protShowCustomButton;
+        [Description("Determines whether or not a custom button will be displayed on a popup form.")]
+        public bool ShowCustomButton
+        {
+            get { return protShowCustomButton; }
+            set { if (value != protShowCustomButton) protShowCustomButton = value; }
+        }
+
+        CustomButtonCollection protButtonsForPopup;
+        [Localizable(true), RefreshProperties(RefreshProperties.All), Description("Returns a collection of custom buttons in the popup window."), 
+        DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public CustomButtonCollection ButtonsForPopupWindow
+        {
+            get { return protButtonsForPopup; }
+        }
+
+
+        // ovverride property
+        public override string EditorTypeName
+        {
+            get { return PopupContainerEditCustomButtonEditorName; }
+        }
+
+        public override void Assign(RepositoryItem item)
+        {
+            base.Assign(item);
+            RepositoryItemPopupContainerEditCustomButton currentRepository = (item as RepositoryItemPopupContainerEditCustomButton);
+            this.ShowCustomButton = currentRepository.ShowCustomButton;
+            this.protButtonsForPopup = currentRepository.protButtonsForPopup;
+            Events.AddHandler(CustomButtonClick, currentRepository.Events[CustomButtonClick]);
+        }
+
+        // let's define delegate and event, fires wgen custom button pressed
+        public delegate void CustomButtonEventHandler(object sender, EventsArgsCustomButton e);
+        [Description("Fires when a custom button is pressed in the popup window."), Category(CategoryName.Events)]
+        public event CustomButtonEventHandler CustomButtonClick;
+    }
+}
---
+++ VB/SimplePopupButton.vb
@@ -0,0 +1,101 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports DevExpress.XtraEditors
+Imports System.Collections
+Imports System.ComponentModel
+
+Namespace PopupContainerCustomButtonControl
+	Public Enum PopupButtonStyle
+		Ok
+		Close
+		Custom
+	End Enum
+
+	Friend Class SimplePopupButton
+		Inherits SimpleButton
+		Private privateDefaultBehavior As PopupButtonStyle
+		Public Property DefaultBehavior() As PopupButtonStyle
+			Get
+				Return privateDefaultBehavior
+			End Get
+			Set(ByVal value As PopupButtonStyle)
+				privateDefaultBehavior = value
+			End Set
+		End Property
+		Public Overrides Function ToString() As String
+			Return Me.DefaultBehavior.ToString()
+		End Function
+	End Class
+
+	Friend Class CustomButtonEventsArgs
+		Inherits EventArgs
+		Protected protCustomButton As SimplePopupButton
+		Public ReadOnly Property CustomButton() As SimplePopupButton
+			Get
+				Return protCustomButton
+			End Get
+		End Property
+		Public Sub New(ByVal paramButton As SimplePopupButton)
+			Me.protCustomButton = paramButton
+		End Sub
+	End Class
+
+	Friend Class CustomButtonCollectionEventArgs
+		Inherits CustomButtonEventsArgs
+		Protected protCurrentAction As CollectionChangeAction
+		Public ReadOnly Property CurrentAction() As CollectionChangeAction
+			Get
+				Return protCurrentAction
+			End Get
+		End Property
+		Public Sub New(ByVal paramButton As SimplePopupButton, ByVal paramAction As CollectionChangeAction)
+			MyBase.New(paramButton)
+			Me.protCurrentAction = paramAction
+		End Sub
+	End Class
+
+
+	<ListBindable(False), TypeConverter(GetType(DevExpress.Utils.Design.UniversalCollectionTypeConverter))> _
+	Friend Class CustomButtonCollection
+		Inherits CollectionBase
+        <Description("Gets a button at the specified position.")> _
+  Default Public ReadOnly Property Item(ByVal index As Integer) As SimplePopupButton
+            Get
+                Return TryCast(List(index), SimplePopupButton)
+            End Get
+        End Property
+
+		Public Overridable Function Add(ByVal button As SimplePopupButton) As Integer
+			Dim res As Integer = IndexOf(button)
+			If res = -1 Then
+				res = List.Add(button)
+			End If
+			Return res
+		End Function
+		Public Overridable Function IndexOf(ByVal button As SimplePopupButton) As Integer
+			Return List.IndexOf(button)
+		End Function
+		Public Overridable Function Contains(ByVal button As SimplePopupButton) As Boolean
+			Return List.Contains(button)
+		End Function
+		Public Overridable Sub Insert(ByVal index As Integer, ByVal button As SimplePopupButton)
+			If Contains(button) Then
+				Return
+			End If
+			List.Insert(index, button)
+		End Sub
+
+		Protected Overrides Sub OnInsertComplete(ByVal index As Integer, ByVal item As Object)
+			RaiseEvent OnCollectionChanged(Me, New CustomButtonCollectionEventArgs((TryCast(item, SimplePopupButton)), CollectionChangeAction.Add))
+		End Sub
+		Protected Overrides Sub OnRemoveComplete(ByVal index As Integer, ByVal item As Object)
+			RaiseEvent OnCollectionChanged(Me, New CustomButtonCollectionEventArgs((TryCast(item, SimplePopupButton)), CollectionChangeAction.Remove))
+		End Sub
+
+		Public Delegate Sub SimpleButtonCollectionEvenrHandler(ByVal sender As Object, ByVal e As CustomButtonCollectionEventArgs)
+		Public Event OnCollectionChanged As SimpleButtonCollectionEvenrHandler
+	End Class
+End Namespace
